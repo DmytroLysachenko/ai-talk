ce62c92af01a160f64f868abd46048dfd5e00e01
DmytroLysachenko <dlysachenko98@gmail.com>
2025-03-31 14:12:33 +0200

    Fixed auth, configured web speech recogn api

--- auth.ts
+++ auth.ts
@@ -1,11 +1,29 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
+import Google from "next-auth/providers/google";
+
+import { createUser, getUserByEmail } from "./lib/actions/auth.action";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google],
   callbacks: {
-    signIn: async ({ user }) => {
-      console.log(user);
-      return true;
+    signIn: async ({ user }: { user: User }) => {
+      const existingUser = await getUserByEmail(user.email!);
+
+      if (existingUser.success && existingUser.user) {
+        return true;
+      }
+
+      const { success } = await createUser({
+        name: user.name!,
+        email: user.email!,
+        avatar: user.image!,
+      });
+
+      if (success) {
+        return true;
+      }
+
+      return false;
     },
   },
 });
--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,5 @@
 import type { NextConfig } from "next";
-import Google from "next-auth/providers/google";
-import { User } from "next-auth";
 
-import { createUser, getUserByEmail } from "./lib/actions/auth.action";
-
-const nextConfig: NextConfig = {
-  providers: [Google],
-  callbacks: {
-    signIn: async ({ user }: { user: User }) => {
-      const existingUser = await getUserByEmail(user.email!);
-
-      if (existingUser.success && existingUser.user) {
-        return true;
-      }
-
-      const { success } = await createUser({
-        name: user.name!,
-        email: user.email!,
-        avatar: user.image!,
-      });
-
-      if (success) {
-        return true;
-      }
-
-      return false;
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
