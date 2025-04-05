183fe3d9fe78de0ee3c1919dc781c1b74db8c37c
DmytroLysachenko <dlysachenko98@gmail.com>
2025-04-05 14:20:44 +0200

    Added some header to FE part.

---
+++ client/components/Header.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+// import { Icons } from "@/components/icons";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Talking AI Pal",
+    href: "/",
+    description:
+      "A talking AI assistant. Perfect for having conversations for english learning",
+  },
+  {
+    title: "Fast text generation from speech.",
+    href: "/",
+    description:
+      "Express everything, what you have on your mind and AI will deal with converting it in a original, structured and easy to read text.",
+  },
+  {
+    title: "Custom purpose AI chatbot",
+    href: "/",
+    description:
+      "Determine the purpose of the conversation and AI will adapt for that purpose.",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background flex justify-center py-4">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
+
+export default Header;
