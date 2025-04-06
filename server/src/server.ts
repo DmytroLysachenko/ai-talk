d34bedf114398f74516f5a7278bd8cb59e5f8cef
DmytroLysachenko <dlysachenko98@gmail.com>
2025-04-06 20:52:44 +0200

    Removed logger.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,40 +2,10 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { KokoroTTS, TextSplitterStream } from "kokoro-js";
-import morgan from "morgan";
-import winston from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${
-        String(timestamp).split(".")[0]
-      } [${level.toUpperCase()}]: ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new DailyRotateFile({
-      filename: "logs/server-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      maxFiles: "14d",
-    }),
-  ],
-});
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.SOCKET_PORT || 3001;
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    morgan("combined", {
-      stream: { write: (message) => logger.info(message.trim()) },
-    })
-  );
-}
 
 const io = new Server(server, {
   cors: {
@@ -47,19 +17,17 @@ const io = new Server(server, {
 let tts: KokoroTTS | null = null;
 (async () => {
   try {
-    logger.info("Loading TTS model...");
     tts = await KokoroTTS.from_pretrained(
       "onnx-community/Kokoro-82M-v1.0-ONNX",
       { dtype: "fp32" }
     );
-    logger.info("✅ TTS Model Loaded!");
   } catch (error) {
-    logger.error("❌ Failed to load TTS Model", error);
+    console.log(error);
   }
 })();
 
 io.on("connection", (socket) => {
-  logger.info("✅ New client connected", socket.id);
+  console.log("✅ New client connected", socket.id);
 
   socket.on("generate_audio", async (message: string) => {
     if (!tts) {
@@ -84,18 +52,18 @@ io.on("connection", (socket) => {
       }
 
       socket.emit("audio_end");
-      logger.info(`✅ Audio streaming completed for message: "${message}"`);
+      console.log(`✅ Audio streaming completed for message: "${message}"`);
     } catch (error) {
-      logger.error("❌ Error in TTS streaming", error);
+      console.log("❌ Error in TTS streaming", error);
       socket.emit("error", "Failed to generate audio.");
     }
   });
 
   socket.on("disconnect", () => {
-    logger.info("❌ Client disconnected", socket.id);
+    console.log("❌ Client disconnected", socket.id);
   });
 });
 
 server.listen(port, () => {
-  logger.info(`🚀 Server running on http://localhost:${port}`);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
